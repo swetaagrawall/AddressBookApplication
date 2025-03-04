ccbaceda6ed59f64b1bd2683b111e10318059a7b
sweta <swetaarawal972@gmail.com>
2025-03-04 10:47:53 +0530

    [Sweta Agrawal]Add. uc1_appSetUp is added sucessfully

---
+++ src/main/java/com/example/addressbookapplication/AddressbookapplicationApplication.java
@@ -0,0 +1,13 @@
+package com.example.addressbookapplication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AddressbookapplicationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AddressbookapplicationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/addressbookapplication/AddressbookapplicationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.addressbookapplication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AddressbookapplicationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
