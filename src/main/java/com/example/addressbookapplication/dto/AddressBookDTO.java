77fd893e17b4f47568dd7288e0af4ccd6752eabf
sweta <swetaarawal972@gmail.com>
2025-03-04 12:25:15 +0530

    [Sweta Agrawal]Add.  uc6_lombokIntegration is added successfully

--- src/main/java/com/example/addressbookapplication/dto/AddressBookDTO.java
+++ src/main/java/com/example/addressbookapplication/dto/AddressBookDTO.java
@@ -1,5 +1,10 @@
 package com.example.addressbookapplication.dto;
+
+import lombok.Data;
+
+@Data
 public class AddressBookDTO {
+    private Long id;
     private String name;
     private String phone;
     private String email;
@@ -10,27 +15,4 @@ public class AddressBookDTO {
         this.email = email;
     }
 
-    public String getPhone() {
-        return phone;
-    }
-
-    public void setPhone(String phone) {
-        this.phone = phone;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
 }
--- src/main/java/com/example/addressbookapplication/model/AddressBook.java
+++ src/main/java/com/example/addressbookapplication/model/AddressBook.java
@@ -6,8 +6,12 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import com.fasterxml.jackson.annotation.JsonPropertyOrder;
 
+import lombok.Getter;
+import lombok.Setter;
 
 @Entity
+@Getter
+@Setter
 @JsonPropertyOrder({"id", "name", "phone", "email"})
 public class AddressBook {
 
@@ -27,35 +31,4 @@ public class AddressBook {
         this.email = email;
     }
 
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public String getPhone() {
-        return phone;
-    }
-
-    public void setPhone(String phone) {
-        this.phone = phone;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
 }
